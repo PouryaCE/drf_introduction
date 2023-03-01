7d03dd261230e81c1b657c59cfa88898f63f91f4
PouryaCE <PouryaJanparvar@yahoo.com>
2023-03-01 18:43:47 +0330

    create our first api for home page

--- core/home/views.py
+++ core/home/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
-from django.views import View
+# from django.shortcuts import render
+# from django.views import View
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
 # Create your views here.
 
 
-class Home(View):
-    def get(self, request):
-        return render(request, 'home/home_page.html')
+# class Home(View):
+#     def get(self, request):
+#         return render(request, 'home/home_page.html')
 
 
+
+# my first api
+@api_view(['GET'])
+def home_page(request):
+    return Response({"name": "pourya","body":"hello from api"})
