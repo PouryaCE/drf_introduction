71ad60185847af3c1bf0d74174e593df77a8ad11
PouryaCE <PouryaJanparvar@yahoo.com>
2023-03-02 22:39:32 +0330

    cleaned models.py

--- core/home/models.py
+++ core/home/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=30)
-    age  = models.PositiveSmallIntegerField()
+    name  = models.CharField(max_length=30)
+    age   = models.PositiveSmallIntegerField()
     email = models.EmailField()
 
     def __str__(self):
