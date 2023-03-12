7536be9f972f643c984c349c1026c5048e2cfbf6
PouryaCE <PouryaJanparvar@yahoo.com>
2023-03-12 12:05:09 +0330

    registered models in admin.py

--- core/home/admin.py
+++ core/home/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Person
+from .models import Person, Answer, Question
 # Register your models here.
 admin.site.register(Person)
+admin.site.register(Answer)
+
+class QuestionAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Question, QuestionAdmin)
