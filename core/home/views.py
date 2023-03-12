850a3381337b318d665eaa9055de6c4dce9bee20
PouryaCE <PouryaJanparvar@yahoo.com>
2023-03-12 11:40:40 +0330

    add a simple permission to our home view

--- core/home/views.py
+++ core/home/views.py
@@ -3,9 +3,10 @@
 # from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response 
-from .models import Person
 from .serializers import PersonSerializer
+from .models import Person
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 
@@ -24,6 +25,9 @@ from django.contrib.auth.models import User
 
 # my second api
 class HomePage(APIView):
+
+    permission_classes = [IsAuthenticated,]
+
     def get(self, request):
         persons = Person.objects.all()
         ser_data = PersonSerializer(instance=persons, many=True)
